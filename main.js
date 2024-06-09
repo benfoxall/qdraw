537339c5e315d1042da2f2ff1516b9f09a5cb9fd
Ben Foxall <benfoxall@gmail.com>
2024-06-09 14:41:11 +0100

    Extract normalise

--- main.js
+++ main.js
@@ -3,27 +3,39 @@ import './style.css'
 const canvas = document.querySelector('#draw')
 const ctx = canvas.getContext('2d')
 
+// state
+const paths = [];
+
+
 canvas.addEventListener('pointermove', e => {
-
-  const { offsetX: x, offsetY: y } = e;
-
-  let rect = canvas.getBoundingClientRect();
-  let scaleX = canvas.width / rect.width;
-  let scaleY = canvas.height / rect.height;
-
-  let scaledX = x * scaleX;
-  let scaledY = y * scaleY;
+  let [x, y] = normalise(e, canvas)
 
   ctx.fillStyle = '#f08'
 
   if (e.pressure === 0) {
-    ctx.fillStyle = '#08f3'
+    ctx.fillStyle = '#08f5'
   }
 
-  ctx.fillRect(scaledX - 5, scaledY - 5, 10, 10);
+  ctx.fillRect(x - 5, y - 5, 10, 10);
 
-  console.log(e)
-  console.log(scaledX, scaledY)
+  const coalescedEvents = e.getCoalescedEvents();
+  for (let coalescedEvent of coalescedEvents) {
+    let [x, y] = normalise(coalescedEvent, canvas)
 
+    ctx.fillStyle = '#fc09'
+    ctx.fillRect(x - 2, y - 2, 4, 4);
+  }
 })
 
+
+function normalise(event, canvas) {
+  let rect = canvas.getBoundingClientRect();
+
+  let scaleX = canvas.width / rect.width;
+  let scaleY = canvas.height / rect.height;
+
+  let x = event.offsetX * scaleX;
+  let y = event.offsetY * scaleY;
+
+  return [x, y]
+}
